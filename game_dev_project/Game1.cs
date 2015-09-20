38a0c900d529262108e54bfe7d850c3a5f7bd9b9
Trevor Carlson <trevor.carlson.mn@gmail.com>
2015-09-20 11:06:51 -0500

    Added player walking and sprinting animations

--- game_dev_project/Game1.cs
+++ game_dev_project/Game1.cs
@@ -11,6 +11,16 @@ namespace game_dev_project
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D walkUp, walkDown, walkLeft, walkRight, sprintUp, sprintDown, sprintLeft, sprintRight,
+            walkUpEquipped, walkDownEquipped, walkLeftEquipped, walkRightEquipped, sprintUpEquipped, sprintDownEquipped,
+            sprintLeftEquipped, sprintRightEquipped, stand, currentAnimation;
+        Rectangle destinationRectangle;
+        Rectangle sourceRectangle;
+        float elapsedTime;
+        float delay = 200f;
+        int frame = 0;
+        KeyboardState ks;
+        Vector2 playerPosition = new Vector2();
 
         public Game1()
         {
@@ -27,7 +37,7 @@ namespace game_dev_project
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            destinationRectangle = new Rectangle(0, 0, 36, 33);
             base.Initialize();
         }
 
@@ -39,6 +49,34 @@ namespace game_dev_project
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // Walk Unequipped
+            walkUp = Content.Load<Texture2D>("Player/walk/walk-up");
+            walkDown = Content.Load<Texture2D>("Player/walk/walk-down");
+            walkLeft = Content.Load<Texture2D>("Player/walk/walk-left");
+            walkRight = Content.Load<Texture2D>("Player/walk/walk-right");
+
+            // Walk Equipped
+            walkUpEquipped = Content.Load<Texture2D>("Player/walk/walk-up-sword");
+            walkDownEquipped = Content.Load<Texture2D>("Player/walk/walk-down-sword");
+            walkLeftEquipped = Content.Load<Texture2D>("Player/walk/walk-left-sword");
+            walkRightEquipped = Content.Load<Texture2D>("Player/walk/walk-right-sword");
+
+            // Sprint Unequipped
+            sprintUp = Content.Load<Texture2D>("Player/sprint/sprint-up");
+            sprintDown = Content.Load<Texture2D>("Player/sprint/sprint-down");
+            sprintLeft = Content.Load<Texture2D>("Player/sprint/sprint-left");
+            sprintRight = Content.Load<Texture2D>("Player/sprint/sprint-right");
+
+            // Sprint Equipped
+            sprintUpEquipped = Content.Load<Texture2D>("Player/sprint/sprint-up-sword");
+            sprintDownEquipped = Content.Load<Texture2D>("Player/sprint/sprint-down-sword");
+            sprintLeftEquipped = Content.Load<Texture2D>("Player/sprint/sprint-left-sword");
+            sprintRightEquipped = Content.Load<Texture2D>("Player/sprint/sprint-right-sword");
+
+            stand = Content.Load<Texture2D>("Player/stand/stand");
+
+            currentAnimation = walkDownEquipped;
 
             // TODO: use this.Content to load your game content here
         }
@@ -52,6 +90,30 @@ namespace game_dev_project
             // TODO: Unload any non ContentManager content here
         }
 
+
+
+        private void Animate(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+            elapsedTime += (float)gameTime.ElapsedGameTime.TotalMilliseconds;
+
+            if (elapsedTime >= delay)
+            {
+                if (frame >= 3)
+                {
+                    frame = 0;
+                }
+                else
+                {
+                    frame++;
+                }
+                elapsedTime = 0;
+            }
+
+            sourceRectangle = new Rectangle(36 * frame, 0, 36, 33);
+        }
+
         /// <summary>
         /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
@@ -62,7 +124,79 @@ namespace game_dev_project
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            destinationRectangle = new Rectangle((int)playerPosition.X, (int)playerPosition.Y, 36, 33);
+            ks = Keyboard.GetState();
+
+
+            // Move Right
+            if (ks.IsKeyDown(Keys.D))
+            {
+                if (ks.IsKeyDown(Keys.LeftShift))
+                {
+                    playerPosition.X += 3f;
+                    currentAnimation = sprintRightEquipped;
+                }
+                else
+                {
+                    playerPosition.X += 1.5f;
+                    currentAnimation = walkRightEquipped;
+                }
+                Animate(gameTime);
+            }
+
+            // Move Left
+            else if (ks.IsKeyDown(Keys.A))
+            {
+                if (ks.IsKeyDown(Keys.LeftShift))
+                {
+                    playerPosition.X -= 3;
+                    currentAnimation = sprintLeftEquipped;
+                }
+                else
+                {
+                    playerPosition.X -= 1.5f;
+                    currentAnimation = walkLeftEquipped;
+                }
+                Animate(gameTime);
+            }
+
+            // Move Up
+            else if (ks.IsKeyDown(Keys.W))
+            {
+                if (ks.IsKeyDown(Keys.LeftShift))
+                {
+                    playerPosition.Y -= 3f;
+                    currentAnimation = sprintUpEquipped;
+                }
+                else
+                {
+                    playerPosition.Y -= 1.5f;
+                    currentAnimation = walkUpEquipped;
+                }
+                Animate(gameTime);
+            }
+
+            // Move Down
+            else if (ks.IsKeyDown(Keys.S))
+            {
+                if (ks.IsKeyDown(Keys.LeftShift))
+                {
+                    playerPosition.Y += 3f;
+                    currentAnimation = sprintDownEquipped;
+                }
+                else
+                {
+                    playerPosition.Y += 1.5f;
+                    currentAnimation = walkDownEquipped;
+                }
+                Animate(gameTime);
+            }
+
+            // Stand
+            else
+            {
+                sourceRectangle = new Rectangle(0, 0, 36, 33);
+            }
 
             base.Update(gameTime);
         }
@@ -76,6 +210,9 @@ namespace game_dev_project
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(currentAnimation, destinationRectangle, sourceRectangle, Color.White);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
